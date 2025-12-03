0749d279b30f63fe02e9026a524aca71257e82c9
Adriana Vincenti <Adrivincenti@outlook.com>
2025-12-03 03:27:14 -0500

    Enemy2

--- Assets/_3DStealthGame/Scripts/Observer.cs
+++ Assets/_3DStealthGame/Scripts/Observer.cs
@@ -36,7 +36,7 @@ public class Observer : MonoBehaviour
             {
                 if (raycastHit.collider.transform == player)
                 {
-                   
+                   gameEnding.CaughtPlayer();
                 }
             }
         }
